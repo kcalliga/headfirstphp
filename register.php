a9c86fc75dae798e18e2981dfd7b7e6524cb8451
kcalliga <41206787+kcalliga@users.noreply.github.com>
2019-07-30 13:10:39 -0400

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<head>
+<title>Submission Results</title>
+</head>
+<body>
+<h2> Test </h2>
+
+<?php
+  $when_it_happened = $_POST['whenithappened'];
+  $how_long = $_POST['howlong'];
+  $alien_description = $_POST['aliendescription'];
+  $fang_spotted = $_POST['fangspotted']
+  $email = $_POST['email'];
+  
+  echo 'Thanks for submitting the form.<br />';
+  echo 'When'. $when_it_happened;
+  ?>
+  </body>
+  </html>
+  
