62e85807378723a15d071ef97dfd896026559950
kcalliga <41206787+kcalliga@users.noreply.github.com>
2019-07-30 12:19:35 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<p>Share your story</p>
+<form method="post" action="report.php">
+  <label for="firstname">First name::</label>
+  <input type="text"> id=firstname" name="firstname" /><br />
+ </form>
+ 
