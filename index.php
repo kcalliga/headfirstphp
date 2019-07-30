2a20b41ce3af8ce3246668e57ccb81c347197ef8
kcalliga <41206787+kcalliga@users.noreply.github.com>
2019-07-30 12:48:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <p>Share your story</p>
 <form method="post" action="report.php">
   <label for="firstname">First name:</label>
-  <input type="text" id=firstname" name="firstname" /><br />
+  <input type="text" id="firstname" name="firstname" /><br />
+  <label for="lastname">Last name:</label>
+  <input type="text" id="lastname" name="lastname" /><br />
+  <label for="email">What is your email address?</label>
+  <input type="text" id="email" name="email" /><br />
+  <label for="whenithappened">When did it happen?</label>
+  <input type="text" id="whenithappeend" name="whenithappeend" /><br />
+  <label for="howlong">How long were you gone?</label>
+  <input type="text" id="howlong" name="howlong" /><br />
+  <label for="howmany">How many did you see?</label>
+  <input type="text" id="howmany" name="howmany" /><br /
+  <label for="aliendescription">Describe them:</label>
+  <input type="text" id="aliendescription" name="aliendescription" /><br /
+  <label for="whattheydid">What did they do to you?</label>
+  <input type="text" id="whattheydid" name="whattheydid" /><br /
+  <label for="fangspotted">Have you seen my dog Fang?</label>
+  Yes <input id="fangspotted" name="fangspotted" type="radio" value="yes" />
+  No <input id="fangspotted" name="fangspotted" type="radio" value="no" />
+  <label for="other">Anything else you want to add?</label>
+  <textarea name="other">/textarea></br>
+  <input type="submit" value="Report" name="submit" />
  </form>
  
