14d6b791cd0d140d15f800b8777cc47450bc292d
kcalliga <41206787+kcalliga@users.noreply.github.com>
2019-08-01 14:09:23 -0400

    Create registeremail.php

---
+++ registeremail.php
@@ -0,0 +1,14 @@
+<html>
+<title> Make Me Elvis.com </title>
+<head> Make Me Elvis.com </head>
+<form method="post" action="addemail.php">
+<label for="firstname">First Name:</label>
+<input type="text" id="firstname" name="firstname" /><br />
+<label for="lastname">Last Name:</label>
+<input type="text" id="lastname" name="lastname" /><br />
+<label for="email">Email:</label>
+<input type="text" id="email" name="email" /><br />
+<input type"submit" name="submit" value="Submit" />
+</form>
+</body>
+</html>
