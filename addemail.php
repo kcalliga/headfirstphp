4fe561c9592ec4a5371217f28efd94e55b324a0b
kcalliga <41206787+kcalliga@users.noreply.github.com>
2019-08-05 13:53:10 -0400

    Update addemail.php

--- addemail.php
+++ addemail.php
@@ -7,22 +7,55 @@
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
+  <img src="blankface.jpg" width="161" height="350" alt="" style="float:right" />
+  <img name="elvislogo" src="elvislogo.gif" width="229" height="32" border="0" alt="Make Me Elvis" />
+  <p>Enter your first name, last name, and email to be added to the <strong>Make Me Elvis</strong> mailing list.</p>
 
 <?php
-  $dbc = mysqli_connect('mysql', 'root', 'password', 'elvis_store')
-    or die('Error connecting to MySQL server.');
+  if (isset($_POST['submit'])) {
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $email = $_POST['email'];
+    $output_form = 'no';
 
-  $first_name = $_POST['firstname'];
-  $last_name = $_POST['lastname'];
-  $email = $_POST['email'];
+    if (empty($first_name) || empty($last_name) || empty($email)) {
+      // We know at least one of the input fields is blank 
+      echo 'Please fill out all of the email information.<br />';
+      $output_form = 'yes';
+    }
+  }
+  else {
+    $output_form = 'yes';
+  }
 
-  $query = "INSERT INTO email_list (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
-  mysqli_query($dbc, $query)
-    or die('Error querying database.');
+  if (!empty($first_name) && !empty($last_name) && !empty($email)) {
+    $dbc = mysqli_connect('mysql', 'root', 'password', 'elvis_store')
+      or die('Error connecting to MySQL server.');
 
-  echo 'Customer added.';
+    $query = "INSERT INTO email_list (first_name, last_name, email)  VALUES ('$first_name', '$last_name', '$email')";
+    mysqli_query($dbc, $query)
+      or die ('Data not inserted.');
 
-  mysqli_close($dbc);
+    echo 'Customer added.';
+
+    mysqli_close($dbc);
+  }
+
+  if ($output_form == 'yes') {
+?>
+
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="firstname">First name:</label>
+    <input type="text" id="firstname" name="firstname" /><br />
+    <label for="lastname">Last name:</label>
+    <input type="text" id="lastname" name="lastname" /><br />
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" /><br />
+    <input type="submit" name="submit" value="Submit" />
+  </form>
+
+<?php
+  }
 ?>
 
 </body>
